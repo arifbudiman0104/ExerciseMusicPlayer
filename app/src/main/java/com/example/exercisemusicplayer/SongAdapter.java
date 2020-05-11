2efbfca596deabf70e41f7a0c14a2bc4d14bd92d
Arif Budiman Arrosyid <arif.budiman.ft18@mail.umy.ac.id>
2020-05-11 15:55:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/exercisemusicplayer/SongAdapter.java
@@ -0,0 +1,54 @@
+package com.example.exercisemusicplayer;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class SongAdapter extends BaseAdapter {
+    private ArrayList<Song> songs;
+    private LayoutInflater songInflater;
+
+    public SongAdapter(Context context, ArrayList<Song> theSongs){
+        songs=theSongs;
+        songInflater=LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return songs.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //map to song layout
+        LinearLayout songLay = (LinearLayout)songInflater.inflate
+                (R.layout.song, parent, false);
+        //get title and artist views
+        TextView songView = (TextView)songLay.findViewById(R.id.song_title);
+        TextView artistView = (TextView)songLay.findViewById(R.id.song_artist);
+        //get song using position
+        Song currSong = songs.get(position);
+        //get title and artist strings
+        songView.setText(currSong.getTitle());
+        artistView.setText(currSong.getArtist());
+        //set position as tag
+        songLay.setTag(position);
+        return songLay;
+    }
+}
